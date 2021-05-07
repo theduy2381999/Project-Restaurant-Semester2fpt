ef0a04a4bd295ea2a170f5d70917804f47cbc204
theduy2381999 <57082572+theduy2381999@users.noreply.github.com>
2021-05-07 23:26:39 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Ngô Hoagfn Thfffffffff
-          Nbennfnsndnnnd mmfmfmff
-        </p>
+        <p>Tao nmoiwf là người đẹp trai nhất nhé THế Duy</p>
         <a
           className="App-link"
           href="https://reactjs.org"
